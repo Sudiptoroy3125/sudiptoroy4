b8b4e99ea7ee7ffb59292e06603257d08408a20a
Sudipto Roy <117767089+Sudiptoroy3125@users.noreply.github.com>
2022-11-09 01:23:59 +0600

    facebook login page clone by tailwind css

---
+++ project 1/tailwind.config.js
@@ -0,0 +1,13 @@
+// // // npx tailwindcss init confSudipto --full
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13: "3.25rem"
+      }
+    },
+  },
+  plugins: [],
+}
